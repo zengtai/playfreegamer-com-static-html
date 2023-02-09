e8955876703a551ce21a21a1554dea3d7d1166e8
Tai Zeng <zengtai@gmail.com>
2023-02-09 13:23:29 +0800

    更新列表项key

--- components/GameList.js
+++ components/GameList.js
@@ -4,7 +4,7 @@ export default function GameList({ items }) {
   return (
     <ul className="game-list">
       {items?.map((i, index) => (
-        <GameListItem key={i?.id} item={i} index={index} />
+        <GameListItem key={i?.appid} item={i} index={index} />
       ))}
     </ul>
   );
